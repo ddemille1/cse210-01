e85d568b175ea56cacc01062ae3fb0483d64cf96
ddemille1 <ddemille1@gmail.com>
2022-01-12 21:38:58 -0500

    everything but squares being taken when using x

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,44 +1,56 @@
 #Tic-Tac-Toe game 
 
-# tasks that i need my program to do:
-
-# create a board
-# display a board 
-
-# prompt the user for their choice
-# switch users after each turn
-
-# update the board with the users choice
-
-# check to see if the user has won after the fifth turn up to the 9th turn
-# check to see if there is a tie on the 9th turn
-
-# end the game after on the 1oth turn
-# display who the winner is
-def main(): 
+def main():
+    win = False
+    tie = False
     turn = 0
-    player = switch_player(turn)
     board = create_board()
     display_board(board)
-    choice = get_choice(player)
-    print(choice)
-
-    turn += 1
+    while win == False and tie == False:
+        player = switch_player(turn)
+        choice = int(get_choice(player, board))
+        updated_board(board, choice, player)
+        display_board(board)
+        win = check_for_win(board, player)
+        tie = check_for_tie(win, turn)
+        turn += 1
 
 def create_board():
     board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     return board
 
 def display_board(board):
+    print('')
     print(f"{board[0]}|{board[1]}|{board[2]}")
     print('-+-+-')
     print(f"{board[3]}|{board[4]}|{board[5]}")
     print('-+-+-')
     print(f"{board[6]}|{board[7]}|{board[8]}")
+    print('')
 
-def get_choice(player):
+def get_choice(player, board):
     choice = int(input(f"{player}'s turn to choose a square (1-9): "))
-    return choice
+    available = check_available(board, choice)
+    if available == True:
+        return choice
+    elif available == False:
+        choice = get_choice(player, board)
+        return choice
+        
+
+def check_available(board, choice):
+    available = True
+    index = choice -1
+    position = board[index]
+    if position == ("X" or "O"):
+        print("That square is taken. Please choose another square")
+        available = False
+        return available
+    else:
+        available = True
+        return available
+
+
 
 def switch_player(turn):
     if (turn % 2) == 0:
@@ -67,12 +79,15 @@ def check_for_win(board, player):
         return win
     else:
         win = False
-        return 
+        return win
         
 def check_for_tie(win, turn):
-    if turn == 9 and win == False:
+    if turn == 8 and win == False:
         print("The game is a tie")
-
+        tie = True
+        return tie
+    tie = False
+    return tie
 
 if __name__ == "__main__":
     main()
